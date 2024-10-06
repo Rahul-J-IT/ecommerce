e24a215f68409c5b0bd212bd7d5652b76ac42449
Rahul-J-IT <rahul22it203@gmail.com>
2024-10-06 17:19:24 +0530

    Fixed some issues and updated functionality

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,11 @@ const express=require('express')
 const { model } = require('mongoose')
 const app=express()
 const products=require('./routes/product')
+const errorMiddleware = require('./middlewares/error')
 
 
 app.use(express.json())
 app.use('/api/v1/',products)
+app.use(errorMiddleware)
 
 module.exports=app
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,10 +1,14 @@
 const express=require('express')
-const { getProducts, newProduct, getSingleProduct } = require('../controllers/ProductController')
+const { getProducts, newProduct, getSingleProduct, updateProduct, deleteProduct } = require('../controllers/ProductController')
 const router=express.Router()
 
 router.route('/products').get(getProducts)
 router.route('/products/new').post(newProduct)
-router.route('/product/:id').get(getSingleProduct)
+router.route('/product/:id')
+                            .get(getSingleProduct)
+                            .put(updateProduct)
+                            .delete(deleteProduct)
+
 
 module.exports=router
 
